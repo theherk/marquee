d4699f925595991d910edfb1463dc36a30dff5fb
Jeff DeFond <jeff.defond@cleardata.com>
2016-10-13 13:38:41 -0700

    Add __init__ override with detail_type kwarg

--- marquee/handler.py
+++ marquee/handler.py
@@ -7,6 +7,20 @@ from marquee.formatter import MarqueeFormatter
 
 
 class CloudWatchEventsHandler(Handler):
+
+    def __init__(self, detail_type=None, *args, **kwargs):
+        """Overridden __init__ method.
+
+        Will set detail_type on handler object then invoke Handler __init__
+        with any passed in arguments
+        """
+        self.detail_type=detail_type
+        super(CloudWatchEventsHandler, self).__init__(
+            *args,
+            **kwargs
+        )
+
+
     def emit(self, record):
         """Overridden emit method.
 
@@ -15,12 +29,13 @@ class CloudWatchEventsHandler(Handler):
         """
         client = boto3.client('events')
         source = self.formatter.source if isinstance(self.formatter, MarqueeFormatter) else __name__
+        detail_type = self.detail_type if self.detail_type is not None else 'marquee'
         return client.put_events(
             Entries=[
                 {
                     'Time': self.formatter.formatTime(record),
                     'Source': source,
-                    'DetailType': 'marquee',
+                    'DetailType': detail_type,
                     'Detail': self.format(record)
                 }
             ]
