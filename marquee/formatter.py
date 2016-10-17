f007053e68b35b35ab29b96cb2944a1b33878a7c
Jeff DeFond <jeff.defond@cleardata.com>
2016-10-17 09:52:26 -0700

    Lint it up

--- marquee/formatter.py
+++ marquee/formatter.py
@@ -2,6 +2,7 @@
 import json
 from logging import Formatter
 
+
 class MarqueeFormatter(Formatter):
     def __init__(self, source=None, *args, **kwargs):
         """marquee logging formatter.
@@ -9,7 +10,7 @@ class MarqueeFormatter(Formatter):
         Args:
             source (str): application from which your are logging
         """
-        self.source = source if source else __name__
+        self.source = source if source is not None else __name__
         super(MarqueeFormatter, self).__init__(*args, **kwargs)
 
     def format(self, record):
@@ -29,7 +30,7 @@ class MarqueeEventFormatter(MarqueeFormatter):
     def __init__(self, event_type=None, source=None, *args, **kwargs):
         """marquee event formatter.
         """
-        self.source = source if source else __name__
+        self.source = source if source is not None else __name__
         self.event_type = event_type if event_type else self.source
         super(MarqueeFormatter, self).__init__(*args, **kwargs)
 
